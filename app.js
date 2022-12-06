a6698d3da9643b8236281969485692ef55be8d02
Imran <Umurzakov117ali@gmail.com>
2022-12-06 17:26:37 +0500

    O'zgaruvchan yaratish va uning xususiyatlari

---
+++ app.js
@@ -0,0 +1,19 @@
+// O'zgaruvchanlar #part1
+
+/*
+  O'zgaruvchanlar dasturlash sohasida juda ko'p joylarda ishlatiladi. O'zgaruvchanlarni asosiy vazifasi biz ishlatadigan malumotlarni saqlab turish yoki uni ozgartirish uchun kerak boladi
+
+  Misol: 1
+  let name = 'Alex'
+  > let - kalit bolib o'zgaruvchini elon qilish uchun kerak boladi
+
+  > name - o'zgaruvchiga beriladigan nom. Batafsil malumot olishingiz uchun #part2 ni korishingiz mumkin.
+
+  > = - yaratilgan o'zgaruvchiga malumotingizni saqlash uchun ishlatiladigan symbol
+
+  Misol: 2
+  name = 'Axe'
+
+  Bu joyda korayotkaningizdek quyidagi ozgaruvchandan biroz farq qilgan holatda huddi shu o'zgaruvchanga boshqa malumot berildi. Ha siz ozingiz yaratkan o'zgaruvchanni qiymatini o'zgartirishingiz mumkin. Lekin eslatib otish joizki siz yaratkan o'zgaruvchanni huddi shu nom bilan qayta yarata olmaysiz.
+  
+*/
