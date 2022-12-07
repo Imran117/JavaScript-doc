effbf0b575b395984fdd87716381d29254f3ca7b
Imran <Umurzakov117ali@gmail.com>
2022-12-07 14:26:37 +0500

    Var eski o'zgaruvchan kaliti

--- app.js
+++ app.js
@@ -1,19 +1,7 @@
-// Konstanta #part3
+// var #part4
 
 /*
-  Bundan oldingi #part1, #part2 mavzularimizada bizlar o'zgaruvchanlarni korib chiqqan edik. Bundan tashqari Js da o'zgarmas saqlovchi ham bor uning nomi konstanta. Agar Matematikani yaxshi bilsangiz oddiy Matematikada ham o'zgarmas qiymatlar bor bunga misol PI soni u o'zgarmas qiymat.
+  Var bu o'zgaruvchan yaratish uchun ishlatilgan kalit so'zi. Lekin shuni bilinki var kalit so'zi eskirgan uslub hisoblanadi Js yarlishidan keyin qo'llanilgan ushbu kalit sozi o'rniga hozirgi davrda let va const ishlatsak bo'ladi.
 
-  Konstanta bilan ishlash juda ham onson lekin shuni esda tutinki konstantaga bergan qiymatingizni qayta chaqirib o'zgartira olmaysiz.
-
-  Misol: 1
-    const age = 15;
-
-    > E'tibor berinki hozir let kaliti sozi o'rniga const kalit so'zi yozilgan shu yo'l bilan konstanta e'lon qilinadi
-
-  Misol: 2
-    age = 'Zafar';
-
-    > Keyingiz misolimizada ko'rishimiza mumkunki quyida e'lon qilgan konstantamizani o'zgartirmoqchi bo'lmoqdamiz lekin bu mutlaqo no to'g'ri.
-
-  Shunday ekan 'konstanta kerak emas, undan foydalanish noqulay deyishga shoshilmang!'. Konstantadan to'g'ri foydalaning uning ichiga keyinchalik umuman o'zgarmaydigan qiymat solib ishlatsangiz sizga juda ham asqotib qolishi mumkin)
+  Keyngi darsdalarimizada aynan var va let kaliti nimasi bilan farq qilishini ko'rib chiqamiza)
 */
